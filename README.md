e69fba688a20f52a15ae00bc1a666eedb181b185
Simeon Kakpovi <skakpovi@gmail.com>
2017-09-25 18:11:01 +0100

    one to one

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Flaskapp10 - One to many database relationships
+# Flaskapp10 - One to One database relationships
 
 Using SQLalchemy.
-Suppose each category has many posts but each post only has one category.
+Suppose each category has many post and each post only has one category.
--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flaskr.db'
@@ -8,15 +9,15 @@ db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
-    categories = Category.query.all()
-    return render_template("index.html", categories=categories)
+    posts = Post.query.all()
+    return render_template("index.html", posts=posts)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(140))
 
     category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
-    category = db.relationship('Category', backref=db.backref('posts', lazy='dynamic'))
+    category = relationship("Category", back_populates="post")
 
     def __init__(self, content, category):
         self.content = content
@@ -29,6 +30,7 @@ class Post(db.Model):
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
+    post = relationship("Post", uselist=False, back_populates="category")
 
     def __init__(self, name):
         self.name = name
@@ -40,19 +42,12 @@ class Category(db.Model):
 @app.route('/addpost', methods=['POST', 'GET'])
 def add():
     content = request.form['content']
-    category = db.session.query(Category).get(request.form['category'])
+    category = Category(request.form['category'])
     post = Post(content, category)
     db.session.add(post)
     db.session.commit()
     return redirect(url_for('index'))
 
-@app.route('/addcategory', methods=['POST'])
-def add_category():
-    name = request.form['category']
-    category = Category(name)
-    db.session.add(category)
-    db.session.commit()
-    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     db.create_all()
